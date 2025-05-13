d4c1ed7788df5be78384f6d3115c44865f2eb4e3
Patryk Kazmierczak <github.scarcity013@passsmail.net>
2025-05-13 15:05:40 +0200

    Add Analysis class for performance testing of data structures and update CMakeLists

---
+++ src/Analysis.hpp
@@ -0,0 +1,21 @@
+#ifndef ANALYSIS_HPP
+#define ANALYSIS_HPP
+#include <map>
+#include <string>
+
+#include "HashMap.hpp"
+class Analysis {
+ private:
+  static constexpr int ITERATIONS = 30;
+  static void printSubTest(int size, int iteration);
+  static void printTestHeader(BucketType type, std::string title);
+  static void writeToFile(const std::string& filename,
+                          const std::map<int, long>& data);
+  static void prepareToTest(int size, int iteration, HashMap& map);
+  static std::map<int, long> analyzeInsert(BucketType bucketType);
+  static std::map<int, long> analyzeRemove(BucketType bucketType);
+ public:
+  void analysis();
+};
+
+#endif  // ANALYSIS_HPP
